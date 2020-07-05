b1ca32905442f28801ba63493da70f9104451144
Naveen Mohideen <43325527+nav610@users.noreply.github.com>
2020-07-05 13:12:06 -0700

    class creation and master recipe list

--- recipe.py
+++ recipe.py
@@ -1,63 +1,28 @@
-class recipe(): 
-	def __init__():
-		self.name = None 
-		self.author = None 
-		self.size = None 
-		self.ingredeintList = None
-		self.stepsList = None 
+from recipeDefn import initRecipe
 
-def makeName(recipe, texts): 
-	recipe.name = texts[0]
-
-def makeSize(recipe,texts):
-	for line in texts: 
-		if 'Serving Size  :' in line:
-			authorString = str(line)
-			after_key = authorString.partition('Serving Size  :')[2]
-			size = after_key.partition('Preparation Time')[0]
-			recipe.size = int(size or None) 
-			break 
-
-def makeAuthor(recipe, texts): 
-	for line in texts: 
-		if 'Recipe By     :' in line: 
-			authorString = str(line)
-			author = authorString.partition('Recipe By     :')[2]
-			if author != None: 
-				recipe.author = author 
-			break 
-
-def makeIngredient(recipe, texts): 
-	for line in texts:
-
-		if 'Amount  Measure       Ingredient -- Preparation Method' == line: 
-			startindex = texts.index(line) + 2
-
-		if 'Nutr. Assoc. :' in line: 
-			endindex = texts.index(line) - 2
-			break 
-
-	ingredeintList = [] 
-	for line in texts[startindex:endindex]: 
-		ingredeintList.append(line)
-
-	stepsList = [] 
-	for line in texts[endindex:endindex+1][0].split('.'): 
-		stepsList.append(line)
-
-
-	recipe.ingredeintList = ingredeintList
-	recipe.stepsList = stepsList 
-
-
-file = open("Korean.txt","r+")
+file = open("AllRecipes.txt","r+")
 
 line_blank_strip = []
 for line in file: 
+	print(line)
 	if line.rstrip(): 
 		line_blank_strip.append(line.strip())
 
-print(line_blank_strip)
+
+startOfRecipe, endOfRecipe = None, None 
+recipes = []
+for i in range(len(line_blank_strip)): 
+	if '* Exported from MasterCook *' == line_blank_strip[i]: 
+		startOfRecipe = i
+
+	if 'Nutr. Assoc. : 0' == line_blank_strip[i]: 
+		endOfRecipe = i + 1
+
+	if startOfRecipe != None and endOfRecipe != None: 
+		recipes.append(initRecipe(line_blank_strip[startOfRecipe:endOfRecipe])) 
+		startOfRecipe, endOfRecipe = None, None
+
+
 
 ### go through the file and consider everything in * Exported to * Exported as one recipe
 ### make recipe object with certain fields and fill those fields in 
---
+++ recipeDefn.py
@@ -0,0 +1,82 @@
+class recipe(): 
+	def __init__(self):
+		self.name = None 
+		self.author = '' 
+		self.size = '' 
+		self.ingredeintList = ''
+		self.stepsList = ''
+
+def makeName(recipe,texts): 
+	recipe.name = texts[1]
+
+
+def makeSize(recipe,texts):
+	for line in texts: 
+		if 'Serving Size  :' in line:
+			authorString = str(line)
+			after_key = authorString.partition('Serving Size  :')[2]
+			size = after_key.partition('Preparation Time')[0]
+			recipe.size = int(size or None) 
+			break 
+
+def makeAuthor(recipe,texts): 
+	for line in texts: 
+		if 'Recipe By     :' in line: 
+			authorString = str(line)
+			author = authorString.partition('Recipe By     :')[2]
+			if author != None: 
+				recipe.author = author 
+			break 
+
+def makeIngredient(recipe,texts): 
+	for line in texts:
+
+		if 'Amount  Measure       Ingredient -- Preparation Method' == line: 
+			startindex = texts.index(line) + 2
+
+		if 'Nutr. Assoc. :' in line: 
+			endindex = texts.index(line) - 2
+			break 
+
+	ingredeintList = [] 
+	for line in texts[startindex:endindex]: 
+		ingredeintList.append(" ".join(line.split()))
+
+	stepsList = [] 
+	for line in texts[endindex:endindex+1][0].split('.'): 
+
+		stepsList.append(" ".join(line.split()))
+
+
+	recipe.ingredeintList = ingredeintList
+	recipe.stepsList = stepsList 
+
+def unpackRecipe(obj): 
+	unpacked = []
+
+	unpacked.append(obj.name)
+	#unpacked.append(obj.author)
+	if obj.size != '': 
+		size = "Serving Size: " + str(obj.size)
+		unpacked.append(size)
+	for line in obj.ingredeintList: 
+		unpacked.append(line)
+	for line in obj.stepsList: 
+		unpacked.append(line)
+
+	new_list = list(filter(None, unpacked))
+	return new_list
+
+
+def initRecipe(texts): 
+	Recipe = recipe()
+	makeName(Recipe, texts)
+	makeAuthor(Recipe, texts)
+	makeIngredient(Recipe, texts)
+	return Recipe
+
+
+
+
+
+
