1d49dd0c459d775953b13f91e66fe63c3724b4a6
Naveen Mohideen <43325527+nav610@users.noreply.github.com>
2020-07-05 18:21:21 -0700

    twitter api and randomization

--- recipe.py
+++ recipe.py
@@ -1,13 +1,20 @@
-from recipeDefn import initRecipe
+from recipeDefn import initRecipe,unpackRecipe
+from requests_oauthlib import OAuth1Session
+from random import shuffle
+#path = './recipe_txts/Cajun.rtf'
+creds = open("creds.txt","r+").readlines()
+session = OAuth1Session(creds[0].strip(),creds[1].strip(),creds[2].strip(),creds[3].strip())
 
-file = open("AllRecipes.txt","r+")
+file = open("AllRecipes.rtf","r+")
 
+count = 0 
 line_blank_strip = []
 for line in file: 
-	print(line)
+	count += 1
 	if line.rstrip(): 
 		line_blank_strip.append(line.strip())
 
+file.close() 
 
 startOfRecipe, endOfRecipe = None, None 
 recipes = []
@@ -24,8 +31,16 @@ for i in range(len(line_blank_strip)):
 
 
 
-### go through the file and consider everything in * Exported to * Exported as one recipe
-### make recipe object with certain fields and fill those fields in 
-### use recipe object to make a pure txt file with everything stripped and tweet ready 
-### go line by line and tweet each recipe 
+random_index = [i for i in range(len(recipes))]
+shuffle(random_index)
+
+#for i in random_index: 
+recipe = recipes[10]
+print(unpackRecipe(recipe))
+
+
+
+#params = {'status':"Hello World!"}
+url = "https://api.twitter.com/1.1/statuses/update.json"
+
 
