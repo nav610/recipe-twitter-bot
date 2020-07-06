85819a42d30d58a7f3eb3559ac9f8d7d6637b8e9
Naveen Mohideen <43325527+nav610@users.noreply.github.com>
2020-07-05 19:47:17 -0700

    posts!

--- recipe.py
+++ recipe.py
@@ -1,6 +1,8 @@
 from recipeDefn import initRecipe,unpackRecipe
 from requests_oauthlib import OAuth1Session
 from random import shuffle
+import time
+
 #path = './recipe_txts/Cajun.rtf'
 creds = open("creds.txt","r+").readlines()
 session = OAuth1Session(creds[0].strip(),creds[1].strip(),creds[2].strip(),creds[3].strip())
@@ -33,14 +35,17 @@ for i in range(len(line_blank_strip)):
 
 random_index = [i for i in range(len(recipes))]
 shuffle(random_index)
-
-#for i in random_index: 
-recipe = recipes[10]
-print(unpackRecipe(recipe))
-
-
-
-#params = {'status':"Hello World!"}
 url = "https://api.twitter.com/1.1/statuses/update.json"
+
+for i in random_index: 
+	recipe = recipes[i]
+	recipe_steps = unpackRecipe(recipe)
+
+	for step in recipe_steps:
+		params = {'status':step}
+		session.post(url, params = params)
+		time.sleep(30)
+
+
 
 
